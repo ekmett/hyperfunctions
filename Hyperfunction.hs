c5d4792ff1d56f3ccdde1e5f2a5e9900b74ae59a
Edward Kmett <ekmett@gmail.com>
2015-06-14 01:39:54 +0200

    instance Strong Hyper. style cleanup

--- Hyperfunction.hs
+++ Hyperfunction.hs
@@ -3,6 +3,7 @@ module Hyperfunctions where
 
 import Control.Arrow
 import Control.Category
+import Control.Monad.Fix
 import Data.Coerce
 
 import Data.Profunctor
@@ -34,21 +35,25 @@ instance Category Hyper where
   f . g = Hyper $ \k -> runHyper f (g . k)
 
 instance Profunctor Hyper where
-  dimap f g h = Hyper (g . runHyper h . dimap g f)
+  dimap f g h = Hyper $ g . runHyper h . dimap g f
 
 instance Arrow Hyper where
-  arr f = r where r = push f r
+  arr = fix . push
   first = ana $ \i fac -> (unroll i (fst . fac), snd (fac i))
   second = ana $ \i fca -> (fst (fca i), unroll i (snd . fca))
   (***) = curry $ ana $ \(i,j) fgac -> (unroll i $ \i' -> fst $ fgac (i',j), unroll j $ \j' -> snd $ fgac (i,j'))
   (&&&) = curry $ ana $ \(i,j) fga  -> (unroll i $ \i' ->       fga  (i',j), unroll j $ \j' ->       fga  (i,j'))
+
+instance Strong Hyper where
+  first' = first
+  second' = second
 
 -- | 
 -- @
 -- push f p . push g q = push (f . g) (p . q)
 -- @
 push :: (a -> b) -> Hyper a b -> Hyper a b
-push f q = Hyper $ \(Hyper k) -> f (k q)
+push f q = Hyper $ \k -> f (runHyper k q)
   
 
 -- |
@@ -66,7 +71,7 @@ base p = Hyper $ \_ -> p
 -- run ('push' f p . q) = f ('run' (q . p)) = f ('runHyper' q p)
 -- @
 run :: Hyper a a -> a
-run (Hyper f) = f id
+run f = runHyper f id
 
 -- |
 -- 'project' is a left inverse for 'arr':
@@ -75,7 +80,7 @@ run (Hyper f) = f id
 -- 'project' '.' 'arr' = 'id'
 -- @
 project :: Hyper a b -> a -> b
-project (Hyper q) x = q (base x)
+project q x = runHyper q (base x)
 
 fold :: [a] -> (a -> b -> c) -> c -> Hyper b c
 fold xs c n = foldr (push . c) (base n) xs
