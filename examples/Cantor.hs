21ebae90a54fe7d9dddeb9887a9e1560ae8ca71b
Edward Kmett <ekmett@gmail.com>
2015-06-18 14:51:23 -0400

    Show that constructive hyperfunctions "Hyper Bool Bool" are isomorphic to the Natural numbers.

---
+++ examples/Cantor.hs
@@ -0,0 +1,129 @@
+{-# LANGUAGE RankNTypes #-}
+module Cantor 
+  ( _Natural
+  ) where
+
+import Control.Monad.Hyper
+import Data.Profunctor
+import Numeric.Natural
+
+-- <http://math.andrej.com/2009/10/12/constructive-gem-double-exponentials/>
+
+data UCF
+  = Const Bool
+  | NonConst UCF'
+
+data UCF'
+  = Head Bool
+  | And Bool UCF'
+  | Or Bool UCF'
+  | Decompose UCF' UCF'
+
+type Cantor = Natural -> Bool
+type Functional = Cantor -> Bool
+
+find :: Functional -> Cantor
+find p = branch x0 l0 (find (\r -> p (branch x0 l0 r)))
+  where
+    branch x _ _ 0 = x
+    branch _ l r n
+      | odd n = l ((n - 1) `div` 2)
+      | otherwise = r ((n - 2) `div` 2)
+
+    x0 = forsome (\l -> forsome (\r -> p (branch True l r)))
+    l0 = find (\l -> forsome (\r -> p (branch x0 l r)))
+
+forevery, forsome :: Functional -> Bool
+forsome f = f (find f)
+forevery f = not (forsome (not . f))
+
+getConst :: Functional -> Maybe Bool
+getConst f 
+  | forevery (\a -> f a == b) = Just b
+  | otherwise = Nothing
+  where b = f (const False)
+    
+prepend :: Bool -> Cantor -> Cantor
+prepend b _ 0 = b
+prepend _ a n = a (n-1)
+
+shift :: Bool -> Functional -> Functional
+shift b f = f . prepend b
+
+fn :: UCF -> Functional
+fn (Const b) = const b
+fn (NonConst x0) = fn' x0 where
+  fn' f a = compute 0 f where
+    compute k (Head b)  = a k == b
+    compute k (And b x) = (a k == b) && compute (k+1) x
+    compute k (Or b x)  = (a k == b) || compute (k+1) x
+    compute k (Decompose x y) 
+      | a k = compute (k+1) y
+      | otherwise = compute (k+1) x
+
+unfn :: Functional -> UCF 
+unfn f0 = case getConst f0 of
+  Just b -> Const b
+  Nothing -> NonConst (unfn' f0)
+    where
+      sf = shift False
+      st = shift True
+      unfn' :: Functional -> UCF'
+      unfn' f = case getConst (sf f) of
+        Nothing -> case getConst (st f) of
+          Nothing -> Decompose (unfn' (sf f)) (unfn' (st f))
+          Just False -> And False (unfn' (sf f))
+          Just True -> Or True (unfn' (sf f))
+        Just False -> case getConst (st f) of
+          Nothing -> And True (unfn' (st f))
+          Just False -> error "impossible"
+          Just True -> Head True
+        Just True -> case getConst (st f) of
+          Nothing -> Or False (unfn' (st f))
+          Just False -> Head False
+          Just True -> error "impossible"
+
+pair :: Natural -> Natural -> Natural
+pair 0 0 = 0
+pair m n = mr + 2 * nr + 4 * pair mq nq where
+  (mq, mr) = divMod m 2
+  (nq, nr) = divMod n 2
+
+unpair :: Natural -> (Natural, Natural)
+unpair 0 = (0, 0)
+unpair n = (xr + 2*x, yr + 2*y) where
+  (p, xr) = divMod n 2
+  (q, yr) = divMod p 2
+  (x, y) = unpair q
+
+enum :: Natural -> UCF
+enum 0 = Const False
+enum 1 = Const True
+enum n0 = NonConst (enum' (n0-2)) where
+  enum' :: Natural -> UCF'
+  enum' 0 = Head False
+  enum' 1 = Head True
+  enum' n | n' <- n - 2, q <- div n' 5 = case mod n' 5 of
+    0 -> And False (enum' q)
+    1 -> And True  (enum' q)
+    2 -> Or False (enum' q)
+    3 -> Or True  (enum' q)
+    _ | (n1, n2) <- unpair q -> Decompose (enum' n1) (enum' n2)
+
+denum :: UCF -> Natural
+denum (Const False) = 0
+denum (Const True) = 1
+denum (NonConst x0) = 2 + denum' x0 where
+  denum' (Head False) = 0
+  denum' (Head True) = 1
+  denum' (And False x) = 2 + 5 * denum' x
+  denum' (And True x) = 3 + 5 * denum' x
+  denum' (Or False x) = 4 + 5 * denum' x
+  denum' (Or True x) = 5 + 5 * denum' x
+  denum' (Decompose x y) = 6 + 5 * pair (denum' x) (denum' y)
+
+type Iso' s a = forall p f. (Profunctor p, Functor f) => p a (f a) -> p s (f s)
+
+-- | Inductive Hyper functions from Bool to Bool are isomorphic to the natural numbers.
+_Natural :: Iso' Natural (Hyper Bool Bool)
+_Natural = dimap (ana (fn . enum)) (fmap (cata (denum . unfn)))
